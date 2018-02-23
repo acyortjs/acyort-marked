c57675e9d3cc51dc70bb4ed161260ac398d86fc3
LoeiFy <LoeiFy@gmail.com>
2018-02-23 23:06:38 +0800

    parse options

--- index.js
+++ index.js
@@ -5,28 +5,10 @@ const formater = require('./lib/format')
 const { Renderer } = marked
 
 class Marked extends Parser {
-  mark(content) {
-    const renderer = Object.assign(new Renderer(), this.parse())
+  parse(content, option = {}) {
+    const renderer = Object.assign(new Renderer(), this.parser(option))
     marked.setOptions({ renderer })
     return marked(content)
-  }
-
-  set config(values) {
-    const {
-      line_numbers: lineNumbers,
-      simple_mode: simpleMode,
-      headingIdFormater,
-    } = values
-
-    if (lineNumbers !== undefined) {
-      this.lineNumbers = lineNumbers
-    }
-    if (simpleMode !== undefined) {
-      this.simpleMode = simpleMode
-    }
-    if (headingIdFormater !== undefined) {
-      this.headingIdFormater = headingIdFormater
-    }
   }
 }
 
