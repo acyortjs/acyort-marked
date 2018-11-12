be7565108f7f8c8bc65cc77769ab024639587535
LoeiFy <LoeiFy@gmail.com>
2018-11-12 21:43:43 +0800

    eslint

--- test/index.js
+++ test/index.js
@@ -1,12 +1,15 @@
+/* global describe it */
+/* eslint no-extend-native: 0 */
+
 const assert = require('power-assert')
 const Marked = require('../')
 
-String.prototype.trim = function() {
+String.prototype.trim = function trim() {
   return this
     .replace(/\n/g, '')
-    .replace(/[\t ]+\</g, '<')
-    .replace(/\>[\t ]+\</g, '><')
-    .replace(/\>[\t ]+$/g, '>')
+    .replace(/[\t ]+</g, '<')
+    .replace(/>[\t ]+</g, '><')
+    .replace(/>[\t ]+$/g, '>')
 }
 
 describe('markdown', () => {
@@ -19,7 +22,7 @@ describe('markdown', () => {
 
     assert(marker.parse(h1).trim() === '<h1><a href="#one" id="one" class="heading"></a>An h1 header</h1>')
 
-    let options = { headingIdFormater: () => 'heading' }
+    const options = { headingIdFormater: () => 'heading' }
     assert(marker.parse(h0, options).trim() === '<h1><a href="#heading" id="heading" class="heading"></a>An h1 header</h1>')
 
     options.simpleMode = true
@@ -32,23 +35,23 @@ describe('markdown', () => {
   it('code', () => {
     let config = { lineNumbers: true }
     let marker = new Marked(config)
-    let code = '\`\`\`html\n<h1>h1</h1>\n\`\`\`'
+    let code = '```html\n<h1>h1</h1>\n```'
 
     assert(marker.parse(code).trim() === '<div class="hljs html"><table><tbody><tr><td class="line"><pre><span>1</span></pre></td><td class="code"><pre><span class="hljs-tag">&lt;<span class="hljs-name">h1</span>&gt;</span>h1<span class="hljs-tag">&lt;/<span class="hljs-name">h1</span>&gt;</span></pre></td></tr></tbody></table></div>')
 
     assert(marker.parse(code, { simpleMode: true }).trim() === '<pre><code>&lt;h1&gt;h1&lt;&#x2F;h1&gt;</code></pre>')
 
-    code = '\`\`\`js\nvar a = 1;\n\`\`\`'
+    code = '```js\nvar a = 1;\n```'
     assert(marker.parse(code, { lineNumbers: false }).trim() === '<div class="hljs javascript"><pre><span class="hljs-attribute">var a</span> = 1;</pre></div>')
 
-    code = '\`\`\`\n<h1>h1</h1>\n\`\`\`'
+    code = '```\n<h1>h1</h1>\n```'
     assert(marker.parse(code, { lineNumbers: false }).trim() === '<div class="hljs"><pre>&lt;h1&gt;h1&lt;&#x2F;h1&gt;</pre></div>')
 
     config = {
       simpleMode: false,
-      lineNumbers: false
+      lineNumbers: false,
     }
-    code = '\`\`\`html\n<h1>h1</h1>\n\`\`\`'
+    code = '```html\n<h1>h1</h1>\n```'
     marker = new Marked(config)
     assert(marker.parse(code).trim() === '<div class="hljs html"><pre><span class="hljs-tag">&lt;<span class="hljs-name">h1</span>&gt;</span>h1<span class="hljs-tag">&lt;/<span class="hljs-name">h1</span>&gt;</span></pre></div>')
   })
